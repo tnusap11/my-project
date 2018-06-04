8a5fb9fd82b1aab8b8e9ac4502341f55a755b051
citz <[tnusap@gmail.com](mailto:tnusap@mail.com)>
2018-06-04 10:01:25 +0800

    什么是动态路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+// 导入组件
+import Test from '@/view/test'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      // 路由参数 :testName
+      path: '/test/:testId/name/:testName',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Test
     }
   ]
 })
---
+++ src/view/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="test">
+        <!-- 获取路由参数 -->
+        this is id : {{$route.params.testId}}
+        <br/>
+        this is name : {{$route.params.testName}}
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
